51680d6e3cbe8316061388d3a778faeea9a6d57d
Rodolfo Rezende <rodolfo.rezende@vetta.digital>
2022-02-22 15:41:36 -0300

    Resolvendo problema com vaclor calculado frontend e backend

--- node_request.mjs
+++ node_request.mjs
@@ -4,28 +4,29 @@ import fetch from 'node-fetch';
 import { readFile } from 'fs/promises';
 
 ( async () => {
-    const response = await fetch("http://localhost:3330/api-gateway/portal/order/1524", {
-    "headers": {
-        "accept": "application/json",
-        "accept-language": "pt-BR",
-        "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFc3RhYmxpc2htZW50SWQiOjEsIldhcmVob3VzZUlkIjo0ODc4LCJpYXQiOjE2NDQ1OTA3MzMsImV4cCI6MTY0NDYwNTEzMywic3ViIjoiYWRtaW4ifQ.njnY5aaUsf8Dc2rya_7z0hlzCD8C5uBQT92tEuK7rUo",
-        "if-none-match": "W/\"24411-h2zAris9qRHX7fvyANUEdtKflPo\"",
-        "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"98\", \"Google Chrome\";v=\"98\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-site",
-        "cookie": "_session.legacy=eCxOdKEsU3yHDbsMLg7OI; _session.legacy.sig=45elUdb_l6qKANDZ5CbTCvGuttM; refreshToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImFkbWluQHdhYnRlYy5jb20iLCJpYXQiOjE2NDQ1OTA3MzMsImV4cCI6MTY0NzE4MjczMywic3ViIjoiYWRtaW4ifQ.OIW4jxHdLC4fXryHQrpO96nZ4B1IfEgWYHo7wFFfNCg",
-        "Referer": "http://localhost:3330/",
-        "Referrer-Policy": "strict-origin-when-cross-origin"
-    },
-    "body": null,
-    "method": "GET"
-    });
+
+    const response = await fetch("http://localhost:3330/api-gateway/portal/order", {
+        "headers": {
+          "accept": "application/json",
+          "accept-language": "pt-BR",
+          "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFc3RhYmxpc2htZW50SWQiOjEsIldhcmVob3VzZUlkIjo0ODc4LCJpYXQiOjE2NDU1NTQzMTksImV4cCI6MTY0NTU2ODcxOSwic3ViIjoiYWRtaW4ifQ.0sHKx9SjbYgmAyN-nDcm734l9TNC-FOHbb2uAEOP-q0",
+          "content-type": "application/json",
+          "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"98\", \"Google Chrome\";v=\"98\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"Windows\"",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+          "cookie": "refreshToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImFkbWluQHdhYnRlYy5jb20iLCJpYXQiOjE2NDU1NTQzMTksImV4cCI6MTY0ODE0NjMxOSwic3ViIjoiYWRtaW4ifQ.ZSjOcN1oZSXiXpvVz-PblKl0oakAYnMXBItJaSYkVVM",
+          "Referer": "http://localhost:3000/",
+          "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": "{\"customerId\":\"5769851\",\"representativeId\":null,\"startDate\":\"2022-01-23 00:00:00\",\"endDate\":\"2022-02-22 23:59:59\",\"pendingApproval\":null}",
+        "method": "POST"
+      });
     
     doSomethingWith(await response.json());
-});
+}); 
 
 
 
@@ -41,10 +42,10 @@ const totalPriceOrder = (products) => {
     let totalPrice = 0;
 
     products.forEach(itemOrder => {
-        totalPrice += (itemOrder.Quantity * (itemOrder.PriceOrigin - ((itemOrder.PriceOrigin * itemOrder.Discount) / 100))) + itemOrder.ValueIpi + itemOrder.ValueST;
+        totalPrice += (itemOrder.Quantity * (itemOrder.PriceOrigin - ((itemOrder.PriceOrigin * itemOrder.Discount) / 100).toFixed(2))) + itemOrder.ValueIpi + itemOrder.ValueST;
     });
 
-    return totalPrice;
+    return totalPrice.toFixed(2);
 };
 
 
