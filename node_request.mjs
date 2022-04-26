453a906771a2a413a9f3c765130ced005389556f
Rodolfo Rezende <rodolfo.rezende@vetta.digital>
2022-04-26 20:32:19 -0300

    saving

--- node_request.mjs
+++ node_request.mjs
@@ -5,25 +5,25 @@ import { readFile } from 'fs/promises';
 
 ( async () => {
 
-    const response = await fetch("http://localhost:3330/api-gateway/portal/order", {
-        "headers": {
-          "accept": "application/json",
-          "accept-language": "pt-BR",
-          "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFc3RhYmxpc2htZW50SWQiOjEsIldhcmVob3VzZUlkIjo0ODc4LCJpYXQiOjE2NDU1NTQzMTksImV4cCI6MTY0NTU2ODcxOSwic3ViIjoiYWRtaW4ifQ.0sHKx9SjbYgmAyN-nDcm734l9TNC-FOHbb2uAEOP-q0",
-          "content-type": "application/json",
-          "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"98\", \"Google Chrome\";v=\"98\"",
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": "\"Windows\"",
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "same-site",
-          "cookie": "refreshToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImFkbWluQHdhYnRlYy5jb20iLCJpYXQiOjE2NDU1NTQzMTksImV4cCI6MTY0ODE0NjMxOSwic3ViIjoiYWRtaW4ifQ.ZSjOcN1oZSXiXpvVz-PblKl0oakAYnMXBItJaSYkVVM",
-          "Referer": "http://localhost:3000/",
-          "Referrer-Policy": "strict-origin-when-cross-origin"
-        },
-        "body": "{\"customerId\":\"5769851\",\"representativeId\":null,\"startDate\":\"2022-01-01 00:00:00\",\"endDate\":\"2022-02-22 23:59:59\",\"pendingApproval\":null}",
-        "method": "POST"
-      });
+    const response = await fetch("http://localhost:3330/api-gateway/portal/customerSite/6006507", {
+      "headers": {
+        "accept": "application/json",
+        "accept-language": "pt-BR",
+        "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFc3RhYmxpc2htZW50SWQiOjEsIldhcmVob3VzZUlkIjo0OTE4LCJpYXQiOjE2NDk3OTA5ODEsImV4cCI6MTY0OTgwNTM4MSwic3ViIjoiZ3VpbGhlcm1lLmVzdGV2ZXMifQ.VbhSe6Jant4_zWCIhBSK1lApcWqrSmF1Bz7G0ad3suA",
+        "if-none-match": "W/\"2852d-DTcq3Zcv8HpGjaCr+gMoH3rtbxU\"",
+        "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"100\", \"Google Chrome\";v=\"100\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "cookie": "_session.legacy=oUrRU5f1xc0Pgs74gzFPr; _session.legacy.sig=teVsftjq8Gujsb-BGQMF10lGM9c; refreshToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6IkdFc3RldmVzQFdhYnRlYy5jb20iLCJpYXQiOjE2NDk3OTA5ODEsImV4cCI6MTY1MjM4Mjk4MSwic3ViIjoiZ3VpbGhlcm1lLmVzdGV2ZXMifQ.XXI5dAF9_wMHaKMil7CvVd5RyrGPjPAKljvOzktboyI",
+        "Referer": "http://localhost:3000/",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+      },
+      "body": null,
+      "method": "GET"
+    });
     
     doSomethingWith(await response.json());
 })(); 
--- test.mjs
+++ test.mjs
@@ -4,22 +4,30 @@ import express from 'express';
 const app = express();
 import http from 'http';
 import request from 'request';
+import axios from 'axios';
 
-const url = 'http://localhost:3002/' //
+const url = /*'https://dev.portal.my.sms-group.com/security/'// */'http://localhost:3002/';
 
+
+/**
+ * To get the authorization code locally, use the following url
+ * http://localhost:3002/api/auth/oidc/auth
+ * To get in dev environment, use
+ * https://dev.portal.my.sms-group.com/security/api/auth/oidc/auth
+ */
 const getToken = async () => {
     try{
         var postData = querystring.stringify(
             {   
                 grant_type: 'authorization_code', 
                 code: process.argv[3], 
-                client_id: 'oidcCLIENT', 
-                client_secret: 'MySecret', 
-                redirect_uri: 'https://oidcdebugger.com/debug' 
+                client_id: '1', 
+                client_secret: 'u7A5S5wo-honqqZ-4ikQ1_Gm0cYMO66cEEnIXZC6ZB9HrZu8XPOL4-5Z97ZXKO2PGMVZIPWkTeY0adkjv-VyUQ', 
+                redirect_uri: 'http://http://oidcdebugger.com/debug'
             }
         );       
-
-        const dataSubmit = await fetchNode(`${url}api/auth/oidc/token?client_id=oidcCLIENT`, 
+        console.log(postData);
+        const dataSubmit = await fetchNode(`${url}api/auth/oidc/token`, 
         {
             "method": "POST",
             headers: {
@@ -52,12 +60,13 @@ const getMe = async (access_token) => {
 }
 
 const registerClient = async () => {
-    const endpoint = 'http://localhost:8080/security/api/auth/oidc/reg';
+    const endpoint = `${url}api/auth/oidc/reg`;
     var options = {
       url: endpoint,
       json: true,
       method: 'POST',
       body: {
+          id: 1,
           grant_types: ["authorization_code", "refresh_token"],      
           redirect_uris: [ 'http://localhost:8083/login/callback'],
           response_types: ["code"],
@@ -65,7 +74,8 @@ const registerClient = async () => {
       },
       headers: {
         //ContentType: 'application/x-www-form-urlencoded'
-        authorization: 'Bearer jhosoyuntokendeacessoinicial'
+        authorization: 'Bearer jhosoyuntokendeacessoinicial',
+       'Cookie': '@sms:token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhZG1pbkB2ZXR0YS5kaWdpdGFsIiwiZmlyc3RfbmFtZSI6IlBsYXRmb3JtIiwibGFzdF9uYW1lIjoiQWRtaW5pc3RyYXRvciIsImlzX2FkbWluIjp0cnVlLCJlbWFpbCI6bnVsbCwic3RhdHVzIjoiQUNUSVZFIiwic2VjdXJpdHkiOnt9LCJpYXQiOjE2NTA2MzI0NzQsImV4cCI6MTY1MDYzMzM3NH0.z_8HSG-wHmoBKGPBv2sscTW6IrZ4yy9BFYDFglLvmmo'
       }
     };
     request.post(options, (err, res, body) => {
@@ -78,12 +88,41 @@ const registerClient = async () => {
   });
 }
 
-console.log(process.argv);
+
+/**
+ * Working code in Customer portal
+ */
+ const oauthFlowInCustomerPortal = async () => {
+    try{
+        const authentication_code = await axios.get(
+            `http://localhost:3002/api/auth/oidc/auth?${querystring.stringify({
+              client_id: '1',
+              response_mode: 'fragment',
+              redirect_uri: 'https://oidcdebugger.com/debug',
+              response_type: 'code',
+              grant_type:'authorization_code',
+              scope: 'openid',
+              client_secret: '2xWMUibc_PoCAtFxqRu8Qs1bXXZRC-XdXPhHlm-xjuabmoHtm8kFUC9d-B1H9Ur6sgjWi1jXlwDxUDO-dBUCeQ'
+            })}`, { headers: { Cookie: '@sms:token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhZG1pbkB2ZXR0YS5kaWdpdGFsIiwiZmlyc3RfbmFtZSI6IlBsYXRmb3JtIiwibGFzdF9uYW1lIjoiQWRtaW5pc3RyYXRvciIsImlzX2FkbWluIjp0cnVlLCJlbWFpbCI6bnVsbCwic3RhdHVzIjoiQUNUSVZFIiwic2VjdXJpdHkiOnt9LCJpYXQiOjE2NTA5ODI1NjcsImV4cCI6MTY1OTk4MjU2N30.8jWhRb1jkttsYhUnJDvRCbl2OF7cjxUWQYyyM5XQAW0'}}
+          );
+        console.log(authentication_code.data);
+        /*const html = document.createElement('div');
+        html.innerHTML = authentication_code.data;
+        console.log(html.querySelector("[name='code']").value);*/
+    } catch (err){
+        console.log(err, 'blakdjwf');
+    }
+    
+}
+
+
+//console.log(process.argv);
 
 switch(process.argv[2]){
     case '1': getToken(); break;
     case '2': getMe(process.argv[3]); break;
     case '3': registerClient();
+    case '4': oauthFlowInCustomerPortal();
 }
 
 app.get('/reg', (req, res, next) => {
@@ -92,6 +131,20 @@ app.get('/reg', (req, res, next) => {
 });
 
 const httpServer = http.createServer(app)
-httpServer.listen(8083,() =>{
+httpServer.listen(8083, () =>{
     console.log(`Http Server Running on port 8083`)
 })
+
+/**
+ * 
+      const result2 = await axios.post(
+        '/security/api/auth/oidc/token',
+        QueryString({
+          client_id: application?.id + '',
+          grant_type: 'authorization_code',
+          redirect_uri: 'http://localhost:3000',
+          client_secret: application?.secret,
+          code: value,
+        }),
+      );
+ */
